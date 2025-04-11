3e904f679a7f82aae00205f5b5e03b6ddf1c132a
ben-yacine <hoelath@gmail.com>
2025-04-11 10:49:34 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 # Build, push on registry and create smartlyai stack
-docker-compose build && docker-compose push && env $(cat .env | grep ^[A-Z] | xargs) docker stack deploy --compose-file docker-compose.yml demo 
+git pull && docker-compose build && docker-compose push && env $(cat .env | grep ^[A-Z] | xargs) docker stack deploy --compose-file docker-compose.yml demo 
