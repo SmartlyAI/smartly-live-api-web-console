9cb1e0b1970447e49c46d3029c3b4b91f151dee4
Hicham TAHIRI <hicham@smartly.ai>
2025-04-11 08:51:39 +0100

    clear mode, refined designed

--- src/components/side-panel/SidePanel.tsx
+++ src/components/side-panel/SidePanel.tsx
@@ -36,13 +36,6 @@ export default function SidePanel() {
   const loggerLastHeightRef = useRef<number>(-1);
   const { log, logs } = useLoggerStore();
 
-  const [textInput, setTextInput] = useState("");
-  const [selectedOption, setSelectedOption] = useState<{
-    value: string;
-    label: string;
-  } | null>(null);
-  const inputRef = useRef<HTMLTextAreaElement>(null);
-
   //scroll the log to the bottom when new logs come in
   useEffect(() => {
     if (loggerRef.current) {
@@ -63,24 +56,10 @@ export default function SidePanel() {
     };
   }, [client, log]);
 
-  const handleSubmit = () => {
-    client.send([{ text: textInput }]);
-
-    setTextInput("");
-    if (inputRef.current) {
-      inputRef.current.innerText = "";
-    }
-  };
-
   return (
     <div className={`side-panel ${open ? "open" : ""}`}>
       <header className="top">
-        <img 
-          src="/assets/img/smartly-ai.png" 
-          alt="Smartly.ai" 
-          style={{ height: '30px' }} 
-          className={cn({ hidden: !open })}
-        />
+        <h2 className={cn({ hidden: !open })}>Logs</h2>
         {open ? (
           <button className="opener" onClick={() => setOpen(false)}>
             <RiSidebarFoldLine color="#b4b8bb" />
@@ -91,75 +70,8 @@ export default function SidePanel() {
           </button>
         )}
       </header>
-      <section className="indicators">
-        <Select
-          className="react-select"
-          classNamePrefix="react-select"
-          styles={{
-            control: (baseStyles) => ({
-              ...baseStyles,
-              background: "var(--Neutral-15)",
-              color: "var(--Neutral-90)",
-              minHeight: "33px",
-              maxHeight: "33px",
-              border: 0,
-            }),
-            option: (styles, { isFocused, isSelected }) => ({
-              ...styles,
-              backgroundColor: isFocused
-                ? "var(--Neutral-30)"
-                : isSelected
-                  ? "var(--Neutral-20)"
-                  : undefined,
-            }),
-          }}
-          defaultValue={selectedOption}
-          options={filterOptions}
-          onChange={(e) => {
-            setSelectedOption(e);
-          }}
-        />
-        <div className={cn("streaming-indicator", { connected })}>
-          {connected
-            ? `📞${open ? " En appel" : ""}`
-            : `📱${open ? " Appel disponible" : ""}`}
-        </div>
-      </section>
       <div className="side-panel-container" ref={loggerRef}>
-        <Logger
-          filter={(selectedOption?.value as LoggerFilterType) || "none"}
-        />
-      </div>
-      <div className={cn("input-container", { disabled: !connected })}>
-        <div className="input-content">
-          <textarea
-            className="input-area"
-            ref={inputRef}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && !e.shiftKey) {
-                e.preventDefault();
-                e.stopPropagation();
-                handleSubmit();
-              }
-            }}
-            onChange={(e) => setTextInput(e.target.value)}
-            value={textInput}
-          ></textarea>
-          <span
-            className={cn("input-content-placeholder", {
-              hidden: textInput.length,
-            })}
-          >
-            Type&nbsp;something...
-          </span>
-
-          <button
-            className="send-button material-symbols-outlined filled"
-            onClick={handleSubmit}
-          >
-            send
-          </button>
-        </div>
+        <Logger filter="none" />
       </div>
     </div>
   );
